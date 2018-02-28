586821159eb3e16aec7c4f4dcbf8ee9b1566206f
skodowski <m.t.skodowski@gmail.com>
2018-02-28 11:58:00 +0100

    spinner

--- src/app/features/posts/post-list/post-list.component.ts
+++ src/app/features/posts/post-list/post-list.component.ts
@@ -30,12 +30,12 @@ export class PostListComponent implements OnInit {
   /* Query multiple collections with realtime listener */
   posts$: Observable<Post[]>;
   subscriptions$: Observable<Post>;
-  observablesArray: Array<Observable<Post>>;
+  arrayOfSubscribedUsers: Array<Observable<Post>>;
   content: string;
   category: string;
 
   constructor(private db: FirebaseService, private auth: AuthService) {
-    this.observablesArray = new Array();
+    this.arrayOfSubscribedUsers = new Array();
     // Query the collection in which the current user saved his subscriptions
     this.subscriptions$ = db.col$(`users/${this.auth.currentUserId}/subscriptions`);
   }
@@ -46,13 +46,13 @@ export class PostListComponent implements OnInit {
         if (data.hasOwnProperty(key)) {
           const element = data[key];
           // Query the documents of the subscribed user and save them in an array
-          this.observablesArray[key] = this.db.colWithIds$(`users/${element.uid}/posts`);
+          this.arrayOfSubscribedUsers[key] = this.db.colWithIds$(`users/${element.uid}/posts`);
         }
       }
       // Combine the arrays with combineLatest
       // As soon as all streams have emitted at least one value
       // each new emission produces a combined value through the result stream
-      this.posts$ = combineLatest<any[]>(...this.observablesArray).pipe(
+      this.posts$ = combineLatest<any[]>(...this.arrayOfSubscribedUsers).pipe(
         // reduce() the values from the source observable
         //
         // concat() (verketten) streams by subscribing
