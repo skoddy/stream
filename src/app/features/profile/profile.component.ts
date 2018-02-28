01fd387f9559f21c1b99ebbce2703fc83da1613e
skodowski <m.t.skodowski@gmail.com>
2018-02-28 12:56:38 +0100

    login

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@app/core';
 
 @Component({
   selector: 'app-profile',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileComponent implements OnInit {
   displayedColumns = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
   }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/auth.service';
 import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
+/** @title Login */
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+  step = 0;
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
