d2278f8257d37a0811d263d483d2bfd27c1e2932
skodowski <m.t.skodowski@gmail.com>
2018-03-01 20:51:46 +0100

    filter

--- src/app/features/user/user-list/user-list.component.ts
+++ src/app/features/user/user-list/user-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '@app/core/firebase.service';
+import { AuthService } from '@app/core/auth.service';
+import { ToastService } from '@app/core/toast.service';
+import { Observable } from '@firebase/util';
+import { filter } from 'rxjs/operator/filter';
 
 export interface Status {
   status: string;
@@ -9,11 +14,18 @@ export interface Status {
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  usersRef: any;
+  users: any;
+  constructor(private db: FirebaseService, private auth: AuthService, private toast: ToastService) {
+    this.usersRef = db.colWithIds$('users');
+    this.users = this.usersRef.map((users) => users.filter(user => user.uid !== this.auth.uid));
 
-  constructor() {
 
   }
-
+/*   getEpic(id: number): Observable<Epic> {
+    return this.getEpics()
+      .map(epics => epics.filter(epic => epic.id !== id)[0]);
+  } */
   ngOnInit() {
   }
 
